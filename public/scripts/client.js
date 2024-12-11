a66f10263b7baa1e052490798dd35cce218acd10
Nesss86 <Nessymonster86@gmail.com>
2024-12-11 10:16:32 -0500

    Added event listner and ajax post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  const $form = $("#tweet-form");
+
+  $form.on("submit", function (event) {
+    event.preventDefault();
+
+    const serializedData = $(this).serialize();
+
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: serializedData,
+    })
+
+    .done(function (response) {
+      console.log("Tweet submitted successfully:", response);
+
+      loadTweets();
+      $form[0].reset();
+    })
+
+    .fail(function (error) {
+      console.error("Error submitting tweet:", error);
+    })
+  });
+});
